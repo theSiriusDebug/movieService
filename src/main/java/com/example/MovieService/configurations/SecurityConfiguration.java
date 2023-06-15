ea0cb19c7117ea8baed148728862d6eb2251ac7d
Sirius M <mihail.chernoby@gmail.com>
2023-06-15 17:30:30 +0300

    update

--- src/main/java/com/example/MovieService/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/MovieService/configurations/SecurityConfiguration.java
@@ -4,13 +4,13 @@ import com.example.MovieService.repositories.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -19,6 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -38,13 +39,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers(
                         "/registration**",
                         "/js/**",
                         "/css/**",
-                        "/img/**"
+                        "/img/**",
+                        "/movies/**",
+                        "/**",
+                        "/static/**"
                 ).permitAll()
+                .antMatchers(HttpMethod.GET, "/movies/create").hasAuthority("ADMIN") // Allow only users with ROLE_ADMIN authority to access "/movies/create" using GET method
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/example/MovieService/controllers/HomeController.java
+++ src/main/java/com/example/MovieService/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package com.example.MovieService.controllers;
 
-import com.example.MovieService.repositories.UserService;
+import com.example.MovieService.models.User;
+import com.example.MovieService.models.dtos.UserRegistrationDto;
+import com.example.MovieService.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import java.util.Optional;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/")
     public String homePage() {
@@ -18,5 +32,50 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/edit")
+    public String editUser(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+
+        Optional<User> existingUserOptional = userRepository.findByUsername(currentUserName);
+        if (existingUserOptional.isEmpty()) {
+            // Обработка ошибки, если пользователь не найден
+            return "error";
+        }
+        User existingUser = existingUserOptional.get();
+
+        model.addAttribute("existingUser", existingUser);
+        model.addAttribute("userRegistrationDto", new UserRegistrationDto());
+
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateUser(@ModelAttribute("userRegistrationDto") UserRegistrationDto userRegistrationDto, Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+
+        Optional<User> existingUserOptional = userRepository.findByUsername(currentUserName);
+        if (existingUserOptional.isEmpty()) {
+            // Обработка ошибки, если пользователь не найден
+            return "error";
+        }
+        User existingUser = existingUserOptional.get();
+
+        existingUser.setLogin(userRegistrationDto.getLogin());
+        existingUser.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+
+        userRepository.save(existingUser);
+
+        model.addAttribute("existingUser", existingUser);
+
+        return "redirect:/home";
+    }
+
+    @GetMapping("/profile")
+    public String profile(){
+        return "profile";
     }
 }
