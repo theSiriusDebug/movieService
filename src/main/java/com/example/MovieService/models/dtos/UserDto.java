1a7afc33976635413dad416dfdf9c5d099771fb2
Sirius M <mihail.chernoby@gmail.com>
2023-06-26 10:58:40 +0300

    update

--- src/main/java/com/example/MovieService/models/dtos/UserDto.java
+++ src/main/java/com/example/MovieService/models/dtos/UserDto.java
@@ -5,10 +5,10 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UserResponseDto {
+public class UserDto {
     private Long id;
     private String username;
-    public UserResponseDto(Long id, String username) {
+    public UserDto(Long id, String username) {
         this.id = id;
         this.username = username;
     }
