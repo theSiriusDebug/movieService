af1bff3fe248e0d185749c4e868cf4682c8c91aa
Kit <70341995+kit52@users.noreply.github.com>
2023-06-28 00:14:17 +0300

    Revert "создан навбар"

--- src/front/src/widgets/Navbar/ui/Navbar.tsx
+++ src/front/src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,11 @@
 
 import React, { memo, useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
-import { Button} from 'antd';
-
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = memo(({ className }: NavbarProps) => {
-    return (
-      <header>
-        <Button className={cls.NavButton}>Register</Button>
-        <Button className={cls.NavButton}>Login</Button>
-      </header>
-    )
-
+    return <h2>Navbar</h2>
 });
