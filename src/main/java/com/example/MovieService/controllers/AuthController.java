815078eb13a6c00c6f422294c01aed1d639371fd
theSiriusDebug <mihail.chernoby@gmail.com>
2023-07-19 20:30:27 +0300

    reworked classes for better readability

--- src/main/java/com/example/MovieService/controllers/AuthController.java
+++ src/main/java/com/example/MovieService/controllers/AuthController.java
@@ -5,20 +5,15 @@ import com.example.MovieService.repositories.UserRepository;
 import com.example.MovieService.repositories.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Api(tags = "Authentication API")
 public class AuthController {
-
     private final UserService userService;
-
-    private static final Logger logger = Logger.getLogger(AuthController.class);
 
     @Autowired
     public AuthController(UserService userService) {
@@ -36,32 +31,7 @@ public class AuthController {
     @ApiOperation("Render login page")
     @GetMapping("/login")
     public String login() {
-        logger.info("Rendering login page");
-
         return "login";
-    }
-
-    @ApiOperation("Show registration form")
-    @GetMapping("/registration")
-    public String showRegistrationForm() {
-        logger.info("Showing registration form");
-
-        return "registration";
-    }
-
-    @ApiOperation("Register a user account")
-    @PostMapping("/registration")
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto) {
-        try {
-            logger.info(String.format("Registering user: %s", userRegistrationDto.getUsername()));
-
-            userService.save(userRegistrationDto, userRegistrationDto.getRole());
-
-            return "redirect:/login";
-        } catch (Exception e) {
-            logger.error(String.format("Error registering user: %s", userRegistrationDto.getUsername(), e));
-            return "error";
-        }
     }
 }
 
---
+++ src/main/java/com/example/MovieService/controllers/UserController.java
@@ -0,0 +1,64 @@
+package com.example.MovieService.controllers;
+
+import com.example.MovieService.models.User;
+import com.example.MovieService.models.dtos.UserRegistrationDto;
+import com.example.MovieService.repositories.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@Api(tags = "UserController")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @ApiOperation("Edit user")
+    @GetMapping("/edit")
+    public ResponseEntity<?> updateUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+
+        Optional<User> existingUserOptional = userRepository.findByUsername(currentUserName);
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+            return ResponseEntity.ok(existingUser);
+        }
+        User existingUser = existingUserOptional.get();
+        return ResponseEntity.ok(existingUser);
+    }
+
+    @ApiOperation("Update user")
+    @PostMapping("/edit")
+    public ResponseEntity<?> updateUser(@RequestBody UserRegistrationDto userRegistrationDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+
+        Optional<User> existingUserOptional = userRepository.findByUsername(currentUserName);
+        if (!existingUserOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        User existingUser = existingUserOptional.get();
+
+        existingUser.setLogin(userRegistrationDto.getLogin());
+        existingUser.setPassword(passwordEncoder.encode(userRegistrationDto.getPassword()));
+
+        userRepository.save(existingUser);
+
+        return ResponseEntity.ok(existingUser);
+    }
+
+}
