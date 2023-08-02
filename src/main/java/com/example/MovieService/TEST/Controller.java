3117e7844936826e7cf6f104768375cd583cfdfe
SiriusM <110127696+theSiriusDebug@users.noreply.github.com>
2023-08-02 21:17:05 +0300

    registration and login with jwt

---
+++ src/main/java/com/example/MovieService/TEST/Controller.java
@@ -0,0 +1,33 @@
+package com.example.MovieService.TEST;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class Controller {
+    private final OkHttpClient client = new OkHttpClient();
+
+    @GetMapping("/popular")
+    public String getPopularMovies() {
+        String url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTkzMjFkNTkxMzkwMDgwMGExYmJiMzc0NTM4NTdmZCIsInN1YiI6IjY0YzVmNjcyNjNlNmZiMDBjNDA5MmYxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gIQ1ZBgljNLDScxFhS5qr9KeQI0wSIOBMJfshRCsFoU")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error occurred during request";
+        }
+    }
+}
--- src/main/java/com/example/MovieService/controllers/ControllerWithTestMethods.java
+++ src/main/java/com/example/MovieService/controllers/ControllerWithTestMethods.java
@@ -4,13 +4,18 @@ import com.example.MovieService.models.Role;
 import com.example.MovieService.models.User;
 import com.example.MovieService.repositories.RoleRepository;
 import com.example.MovieService.repositories.UserRepository;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/test")
+@Api(tags = "TestController API")
 public class ControllerWithTestMethods {
 
     private UserRepository userRepository;
@@ -22,12 +27,12 @@ public class ControllerWithTestMethods {
         this.roleRepository = roleRepository;
     }
 
-    @GetMapping("/usertest")
+    @GetMapping("/user")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userRepository.findAll());
     }
 
-    @GetMapping("/roletest")
+    @GetMapping("/role")
     public ResponseEntity<List<Role>> getRoles() {
         return ResponseEntity.ok(roleRepository.findAll());
     }
