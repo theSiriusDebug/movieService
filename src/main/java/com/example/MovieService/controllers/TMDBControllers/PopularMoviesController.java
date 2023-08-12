7875484e7d908ce751f3e91f4bf7ffc580702968
siriusm <t@gmail.com>
2023-08-12 11:00:45 +0300

    logger

--- src/main/java/com/example/MovieService/controllers/TMDBControllers/PopularMoviesController.java
+++ src/main/java/com/example/MovieService/controllers/TMDBControllers/PopularMoviesController.java
@@ -10,18 +10,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PopularMoviesController  {
-    private final OkHttpClient client = new OkHttpClient();
+    private final OkHttpClient httpClient = new OkHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private MovieRepository movieRepository;
-    private GenreRepository genreRepository;
+    private final MovieRepository movieRepository;
+    private final GenreRepository genreRepository;
+    private static final Logger logger = LoggerFactory.getLogger(PopularMoviesController.class);
 
     @Autowired
     public PopularMoviesController (MovieRepository movieRepository, GenreRepository genreRepository) {
@@ -32,63 +37,72 @@ public class PopularMoviesController  {
     @GetMapping("/addMovies")
     public List<Movie> addPopularMoviesToDB() {
         List<Movie> movies = new ArrayList<>();
-        boolean a = true;
-        int page = 1;
-        while (a == true) {
-            if (page>=10){
-                a=false;
-            }
-            String url = String.format("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=%s", page);
+        boolean continueFetching = true;
+        long currentPage = 1;
+        long maxPages = 1;
+        while (continueFetching && currentPage < maxPages) {
+            String apiUrl = String.format("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=%d", currentPage);
             Request request = new Request.Builder()
-                    .url(url)
+                    .url(apiUrl)
                     .get()
                     .addHeader("accept", "application/json")
                     .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTkzMjFkNTkxMzkwMDgwMGExYmJiMzc0NTM4NTdmZCIsInN1YiI6IjY0YzVmNjcyNjNlNmZiMDBjNDA5MmYxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gIQ1ZBgljNLDScxFhS5qr9KeQI0wSIOBMJfshRCsFoU")
                     .build();
 
             try {
-                Response response = client.newCall(request).execute();
+                Response response = httpClient.newCall(request).execute();
                 String responseBody = response.body().string();
                 MovieApiResponse apiResponse = objectMapper.readValue(responseBody, MovieApiResponse.class);
-                movies = mapToMovies(apiResponse);
+                movies = mapApiResponseToMovies(apiResponse);
                 movieRepository.saveAll(movies);
-                page++;
-            } catch (IOException e) {
-                e.printStackTrace();
-                page++;
-            }
-        }
+                logger.info("Fetched and saved movies for page {}", currentPage);
+                currentPage++;
+            } catch (HttpClientErrorException e) {
+                logger.error("An error occurred while processing the API response: {}", e.getResponseBodyAsString(), e);
+            } catch (Exception e) {
+                logger.error("An error occurred: {}", e.getMessage(), e);
+            }        }
         return movies;
     }
 
-    private List<Movie> mapToMovies(MovieApiResponse apiResponse) {
-        List<Movie> movies = new ArrayList<>();
+    private List<Movie> mapApiResponseToMovies(MovieApiResponse apiResponse) {
+        List<Movie> mappedMovies = new ArrayList<>();
         for (MovieApiItem apiItem : apiResponse.getResults()) {
-            Movie movie = new Movie();
-            movie.setTitle(apiItem.getTitle());
-            movie.setDescription(apiItem.getOverview());
-            movie.setRelease_year(apiItem.getRelease_date());
-            movie.setDirector(apiItem.getDirector());
-
-            List<Genre> genres = new ArrayList<>();
-            for (Long genreId : apiItem.getGenre_ids()) {
-                Genre genre = genreRepository.findById(genreId).orElse(null);
-                if (genre != null) {
-                    genres.add(genre);
-                } else {
-                    System.out.println("Genre not found for ID: " + genreId);
-                }
-            }
-            movie.setGenres(genres);
-
-            movie.setDuration(apiItem.getRuntime());
-            movie.setRating(apiItem.getVote_average());
-            movie.setCoverImage(apiItem.getPoster_path());
-            movie.setHorizontal_image(apiItem.getPoster_path());
-            movie.setVideo(apiItem.getVideo_path());
-            movie.setTrailer(apiItem.getTrailer_path());
-            movies.add(movie);
+            mappedMovies.add(mapApiItemToMovie(apiItem));
         }
-        return movies;
+        return mappedMovies;
+    }
+
+    private Movie mapApiItemToMovie(MovieApiItem apiItem) {
+        Movie movie = new Movie();
+        movie.setTitle(apiItem.getTitle());
+        movie.setDescription(apiItem.getOverview());
+        movie.setRelease_year(apiItem.getRelease_date());
+        movie.setDirector(apiItem.getDirector());
+
+        List<Genre> genres = mapGenreIdsToGenres(apiItem.getGenre_ids());
+        movie.setGenres(genres);
+
+        movie.setDuration(apiItem.getRuntime());
+        movie.setRating(apiItem.getVote_average());
+        movie.setCoverImage(apiItem.getPoster_path());
+        movie.setHorizontal_image(apiItem.getPoster_path());
+        movie.setVideo(apiItem.getVideo_path());
+        movie.setTrailer(apiItem.getTrailer_path());
+
+        return movie;
+    }
+
+    private List<Genre> mapGenreIdsToGenres(List<Long> genreIds) {
+        List<Genre> genres = new ArrayList<>();
+        for (Long genreId : genreIds) {
+            Genre genre = genreRepository.findById(genreId).orElse(null);
+            if (genre != null) {
+                genres.add(genre);
+            } else {
+                logger.warn("Genre not found for ID: {}", genreId);
+            }
+        }
+        return genres;
     }
 }
--- src/main/java/com/example/MovieService/controllers/authControllers/RegistrationController.java
+++ src/main/java/com/example/MovieService/controllers/authControllers/RegistrationController.java
@@ -7,6 +7,8 @@ import com.example.MovieService.models.dtos.UserRegistrationDto;
 import com.example.MovieService.repositories.RoleRepository;
 import com.example.MovieService.sevices.UserService;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,7 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 @Api(tags = "RegistrationController API")
 public class RegistrationController {
+    private static final Logger logger = LoggerFactory.getLogger(RegistrationController.class);
     private AuthenticationManager authenticationManager;
     private UserService userService;
     private JwtTokenProvider jwtTokenProvider;
@@ -38,7 +41,9 @@ public class RegistrationController {
     public ResponseEntity<?> register(@RequestBody UserRegistrationDto registrationDto) {
         try {
             String username = registrationDto.getUsername();
+            logger.info("Registration attempt for username: {}", username);
             if (userService.findByUsername(username) == null) {
+                logger.info("Username '{}' is available for registration", username);
                 User newUser = new User();
                 newUser.setUsername(username);
                 newUser.setPassword(new BCryptPasswordEncoder().encode(registrationDto.getPassword()));
@@ -51,11 +56,15 @@ public class RegistrationController {
                 Map<Object, Object> response = new HashMap<>();
                 response.put("username", newUser.getUsername());
                 response.put("token", token);
+
+                logger.info("User registered successfully: {}", newUser.getUsername());
                 return ResponseEntity.ok(response);
             } else {
+                logger.warn("Registration attempt failed - Username already exists: {}", username);
                 return ResponseEntity.badRequest().body("Username already exists");
             }
         } catch (Exception e) {
+            logger.error("An error occurred during user registration: {}", e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
