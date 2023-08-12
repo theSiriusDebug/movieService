c170c8bdc01dea8d39fe616a9bcc0c94c62f0b25
siriusm <t@gmail.com>
2023-08-12 12:04:17 +0300

    logger and Tests

--- src/main/java/com/example/MovieService/models/dtos/AuthDto.java
+++ src/main/java/com/example/MovieService/models/dtos/AuthDto.java
@@ -1,9 +1,11 @@
 package com.example.MovieService.models.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class AuthDto {
     private String username;
