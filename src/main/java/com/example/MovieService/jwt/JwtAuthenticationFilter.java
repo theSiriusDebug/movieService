6bf90e73a675e11941e4cd873ac742e3beccadb3
siriusm <t@gmail.com>
2023-08-13 11:17:24 +0300

    log

--- src/main/java/com/example/MovieService/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/MovieService/jwt/JwtAuthenticationFilter.java
@@ -3,7 +3,10 @@ package com.example.MovieService.jwt;
 import com.example.MovieService.models.User;
 import com.example.MovieService.models.dtos.AuthDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,6 +22,8 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
@@ -28,8 +33,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
             AuthDto authRequest = new ObjectMapper().readValue(request.getInputStream(), AuthDto.class);
-            System.out.println("Username: " + authRequest.getUsername());
-            System.out.println("Password: " + authRequest.getPassword());
+            logger.info("Attempting authentication for user: {}", authRequest.getUsername());
 
             Authentication authentication = new UsernamePasswordAuthenticationToken(
                     authRequest.getUsername(),
@@ -38,14 +42,17 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             );
             return authenticationManager.authenticate(authentication);
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            logger.error("Error reading authentication request", e);
+            throw new AuthenticationServiceException("Error reading authentication request", e);        }
     }
-
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
-        String token = jwtTokenProvider.createToken(((User) authResult.getPrincipal()).getUsername(), ((User) authResult.getPrincipal()).getRoles());
+        User principal = (User) authResult.getPrincipal();
+        String token = jwtTokenProvider.createToken(principal.getUsername(), principal.getRoles());
+
+        logger.info("Successful authentication for user: {}", principal.getUsername());
+
         response.addHeader("Authorization", "Bearer " + token);
     }
 }
