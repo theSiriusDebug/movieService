020648ca177206ccd6f378070c78570b666be72c
theSiriusDebug <misha.ch2006@gmail.com>
2023-10-27 10:43:48 +0300

    added endpoints that return a list of users.

--- src/main/java/com/example/MovieService/controllers/UserControllers/UserController.java
+++ src/main/java/com/example/MovieService/controllers/UserControllers/UserController.java
@@ -11,12 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Api(tags = "UserController")
@@ -62,8 +60,13 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
     }
 
-    @GetMapping("/usertest")
-    public ResponseEntity<List<User>> getUsers() {
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> get(){
         return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Optional<User>> get(@PathVariable("id") long id){
+        return ResponseEntity.ok(userRepository.findById(id));
     }
 }
