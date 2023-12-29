4bfca60fcd24f6ab9e97854775635e70471ea8f4
theSiriusDebug <misha.ch2006@gmail.com>
2023-12-29 14:35:04 +0200

    added UserService, MovieService to WatchLaterMoviesController and add new method into MovieService.

--- src/main/java/com/example/MovieService/controllers/MovieControllers/WatchLaterMoviesController.java
+++ src/main/java/com/example/MovieService/controllers/MovieControllers/WatchLaterMoviesController.java
@@ -2,8 +2,8 @@ package com.example.MovieService.controllers.MovieControllers;
 
 import com.example.MovieService.models.Movie;
 import com.example.MovieService.models.User;
-import com.example.MovieService.repositories.MovieRepository;
-import com.example.MovieService.repositories.UserRepository;
+import com.example.MovieService.sevices.MovieService;
+import com.example.MovieService.sevices.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -19,26 +19,26 @@ import java.util.List;
 
 @Api(tags = "WatchLaterMoviesController API")
 @RestController
-@RequestMapping
+@RequestMapping("/watchLaterMovies")
 public class WatchLaterMoviesController {
-    private final UserRepository userRepository;
-    private final MovieRepository movieRepository;
+    private final UserService userService;
+    private final MovieService movieService;
     private static final Logger logger = LoggerFactory.getLogger(WatchLaterMoviesController.class);
 
     @Autowired
-    public WatchLaterMoviesController(UserRepository userRepository, MovieRepository movieRepository) {
-        this.userRepository = userRepository;
-        this.movieRepository = movieRepository;
+    public WatchLaterMoviesController(UserService userService, MovieService movieService) {
+        this.userService = userService;
+        this.movieService = movieService;
     }
 
     @ApiOperation("add movie to watch later list")
-    @PostMapping("/watchLaterMovies/{movieId}")
+    @PostMapping("/{movieId}")
     public ResponseEntity<String> addWatchLaterMovie(@PathVariable("movieId") long movieId) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByUsername(authentication.getName());
+        User user = userService.findByOptionalUsername(authentication.getName());
 
-        Movie movie = movieRepository.findById(movieId);
+        Movie movie = movieService.findOptionalMovieById(movieId);
         if (movie == null) {
             logger.error("Movie with ID {} not found.", movieId);
             throw new EntityNotFoundException("Movie not found");
@@ -47,7 +47,7 @@ public class WatchLaterMoviesController {
         List<Movie> watchLaterMovies = user.getWatchLaterMovies();
         if (!watchLaterMovies.contains(movie)) {
             watchLaterMovies.add(movie);
-            userRepository.save(user);
+            userService.save(user);
             logger.info("Movie with ID {} added to watch later for user {}.", movieId, user.getUsername());
         }
 
@@ -55,13 +55,13 @@ public class WatchLaterMoviesController {
     }
 
     @ApiOperation("remove movie from watch later list")
-    @DeleteMapping("/watchLaterMovies/{movieId}")
+    @DeleteMapping("/{movieId}")
     public ResponseEntity<String> removeWatchLaterMovie(@PathVariable("movieId") long movieId) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByUsername(authentication.getName());
+        User user = userService.findByUsername(authentication.getName());
 
-        Movie movie = movieRepository.findById(movieId);
+        Movie movie = movieService.findOptionalMovieById(movieId);
         if (movie == null) {
             logger.error("Movie with ID {} not found.", movieId);
             throw new EntityNotFoundException("Movie not found");
@@ -70,7 +70,7 @@ public class WatchLaterMoviesController {
         List<Movie> watchLater = user.getWatchLaterMovies();
         if (watchLater.contains(movie)) {
             watchLater.remove(movie);
-            userRepository.save(user);
+            userService.save(user);
             logger.info("Movie with ID {} removed from watch later for user {}.", movieId, user.getUsername());
         }
 
--- src/main/java/com/example/MovieService/sevices/MovieService.java
+++ src/main/java/com/example/MovieService/sevices/MovieService.java
@@ -1,13 +1,16 @@
 package com.example.MovieService.sevices;
 
 import com.example.MovieService.models.Movie;
+import com.example.MovieService.models.User;
 import com.example.MovieService.repositories.MovieRepository;
 import javassist.NotFoundException;
 import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
@@ -26,6 +29,15 @@ public class MovieService {
 
     public Movie findMovieById(long id) {
         return movieRepository.findById(id);
+    }
+
+    public Movie findOptionalMovieById(long id) {
+        try {
+            return Optional.ofNullable(movieRepository.findById(id))
+                    .orElseThrow(() -> new NotFoundException("Movie not found"));
+        } catch (NotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void deleteMovie(Movie movie) {
