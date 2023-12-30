8971f0970c1aa0460f6b5d1265a05e88be57ee21
theSiriusDebug <misha.ch2006@gmail.com>
2023-12-30 23:59:51 +0200

    added interface ReviewService.

---
+++ src/main/java/com/example/MovieService/sevices/interfaces/ReviewService.java
@@ -0,0 +1,18 @@
+package com.example.MovieService.sevices.interfaces;
+
+import com.example.MovieService.models.Movie;
+import com.example.MovieService.models.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+    List<Review> findAllReviews();
+
+    Review findReviewById(long id);
+
+    void deleteReview(Review review);
+
+    List<Review> findReviewByMovie(Movie movie);
+
+    void saveReview(Review review);
+}
