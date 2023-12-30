9287913588babe8d95efa7e84493a9e798c01ce4
theSiriusDebug <misha.ch2006@gmail.com>
2023-12-30 15:47:39 +0200

    added UserService and MovieService to RatingCreationController.

--- src/main/java/com/example/MovieService/controllers/MovieControllers/ReviewControllers/RatingCreationController.java
+++ src/main/java/com/example/MovieService/controllers/MovieControllers/ReviewControllers/RatingCreationController.java
@@ -3,8 +3,8 @@ package com.example.MovieService.controllers.MovieControllers.ReviewControllers;
 import com.example.MovieService.models.Movie;
 import com.example.MovieService.models.Rating;
 import com.example.MovieService.models.User;
-import com.example.MovieService.repositories.MovieRepository;
-import com.example.MovieService.repositories.UserRepository;
+import com.example.MovieService.sevices.MovieService;
+import com.example.MovieService.sevices.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +19,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/rating")
 public class RatingCreationController {
 
-    private final MovieRepository movieRepository;
-    private final UserRepository userRepository;
+    private final MovieService movieService;
+    private final UserService userService;
 
     @Autowired
-    public RatingCreationController(MovieRepository movieRepository, UserRepository userRepository) {
-        this.movieRepository = movieRepository;
-        this.userRepository = userRepository;
+    public RatingCreationController(MovieService movieService, UserService userService) {
+        this.movieService = movieService;
+        this.userService = userService;
     }
 
     @ApiOperation("Create a rating")
     @PostMapping("/create/{movieId}")
     public ResponseEntity<String> createRating(@PathVariable Long movieId, @RequestBody Rating rating) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByUsername(authentication.getName());
+        User user = userService.findByOptionalUsername(authentication.getName());
 
-        Movie movie = movieRepository.findById(movieId).orElse(null);
-
-        if (movie == null || user == null) {
-            return new ResponseEntity<>("Movie or User not found.", HttpStatus.NOT_FOUND);
-        }
+        Movie movie = movieService.findOptionalMovieById(movieId);
 
         for (Rating existingRating : user.getRating()) {
             if (existingRating.getMovie().getId().equals(movieId)) {
@@ -51,7 +47,7 @@ public class RatingCreationController {
         rating.setRatingValue(rating.getRatingValue());
         user.getRating().add(rating);
 
-        userRepository.save(user);
+        userService.save(user);
 
         return new ResponseEntity<>("Rating created successfully.", HttpStatus.CREATED);
     }
