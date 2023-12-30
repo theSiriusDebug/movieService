ac10e1d0277ba8b841fa7196919a18b79f9141e9
theSiriusDebug <misha.ch2006@gmail.com>
2023-12-30 15:48:16 +0200

    added MovieService to StreamMovieController.

--- src/main/java/com/example/MovieService/controllers/MovieControllers/StreamMovieController.java
+++ src/main/java/com/example/MovieService/controllers/MovieControllers/StreamMovieController.java
@@ -1,7 +1,7 @@
 package com.example.MovieService.controllers.MovieControllers;
 
 import com.example.MovieService.models.Movie;
-import com.example.MovieService.repositories.MovieRepository;
+import com.example.MovieService.sevices.MovieService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/movies/watch")
 @Api(tags = "MovieStreamingController API")
 public class StreamMovieController {
-    private final MovieRepository movieRepository;
+    private final MovieService movieService;
 
     @Autowired
-    public StreamMovieController(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
+    public StreamMovieController(MovieService movieService) {
+        this.movieService = movieService;
     }
 
     @ApiOperation("Watch movie by movie ID")
     @GetMapping("/{id}")
     public ResponseEntity<String> getMovieVideoByMovieId(@PathVariable long movieId) {
-        Movie movieById = movieRepository.findById(movieId);
+        Movie movieById = movieService.findOptionalMovieById(movieId);
         if (movieById != null) {
             String videoUrl = movieById.getMovieLink();
             return ResponseEntity.ok(videoUrl);
@@ -34,7 +34,7 @@ public class StreamMovieController {
     @ApiOperation("Watch movie by trailer ID")
     @GetMapping("/trailer/{id}")
     public ResponseEntity<String> getTrailerByMovieId(@PathVariable long movieId) {
-        Movie movieById = movieRepository.findById(movieId);
+        Movie movieById = movieService.findOptionalMovieById(movieId);
         if (movieById != null) {
             String videoUrl = movieById.getTrailerLink();
             return ResponseEntity.ok(videoUrl);
