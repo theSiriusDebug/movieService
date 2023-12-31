bc97c707301fe37df721971fe540f78ddf34f8da
theSiriusDebug <misha.ch2006@gmail.com>
2023-12-31 21:03:48 +0200

    rebuild jar.

--- src/main/java/com/example/MovieService/sevices/interfaces/UserService.java
+++ src/main/java/com/example/MovieService/sevices/interfaces/UserService.java
@@ -1,11 +1,6 @@
 package com.example.MovieService.sevices.interfaces;
 
-import com.example.MovieService.models.Movie;
-import com.example.MovieService.models.Role;
 import com.example.MovieService.models.User;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Set;
 
 public interface UserService {
     User findByUsername(String username);
