28d3f0303ecdbff290c860a04df5ef185f8dfb67
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-02 19:18:09 +0200

    RoleServiceImpl started using annotation @Slf4j instead of class Logger.

--- src/main/java/com/example/MovieService/sevices/RoleServiceImpl.java
+++ src/main/java/com/example/MovieService/sevices/RoleServiceImpl.java
@@ -3,17 +3,17 @@ package com.example.MovieService.sevices;
 import com.example.MovieService.models.Role;
 import com.example.MovieService.repositories.RoleRepository;
 import com.example.MovieService.sevices.interfaces.RoleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Logger;
 
 @Service
+@Slf4j
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
-    private static final Logger logger = Logger.getLogger(ReviewServiceImpl.class.getName());
 
     @Autowired
     public RoleServiceImpl(RoleRepository roleRepository) {
@@ -22,14 +22,14 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public List<Role> findAllRoles() {
-        logger.info("Retrieving all roles");
+        log.info("Retrieving all roles");
         return roleRepository.findAll();
     }
 
     @Override
     public void saveRole(Role role) {
         roleRepository.save(Objects.requireNonNull(role, "Role cannot be null."));
-        logger.info("Role saved successfully.");
+        log.info("Role saved successfully.");
     }
 
     @Override
