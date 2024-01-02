0f8affc31385e0d3c2e3bc4100fb622def5791dc
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-02 19:05:52 +0200

    MovieServiceImpl started using annotation @Slf4j instead of class Logger.

--- src/main/java/com/example/MovieService/sevices/MovieServiceImpl.java
+++ src/main/java/com/example/MovieService/sevices/MovieServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.MovieService.models.Movie;
 import com.example.MovieService.repositories.MovieRepository;
 import com.example.MovieService.sevices.interfaces.MovieService;
 import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,9 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
+@Slf4j
 public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieRepository;
-    private static final Logger logger = Logger.getLogger(MovieServiceImpl.class.getName());
 
     @Autowired
     public MovieServiceImpl(MovieRepository movieRepository) {
@@ -23,41 +24,41 @@ public class MovieServiceImpl implements MovieService {
     }
 
     public List<Movie> findAllMovies(){
-        logger.info("Return all movies");
+        log.info("Return all movies");
         return movieRepository.findAll();
     }
 
     @Override
     public Movie findMovieById(long id) {
-        logger.info("Find movie by id " + id);
+        log.info("Find movie by id " + id);
         return movieRepository.findById(id);
     }
 
     @Override
     public Movie findOptionalMovieById(long id) {
-        logger.info(String.format("Searching for movie with ID: %s", id));
+        log.info(String.format("Searching for movie with ID: %s", id));
 
         try {
             Movie movie = Optional.ofNullable(movieRepository.findById(id))
                     .orElseThrow(() -> new NotFoundException("Movie not found"));
-            logger.info(String.format("Movie found: %s", movie));
+            log.info(String.format("Movie found: %s", movie));
             return movie;
         } catch (NotFoundException e) {
-            logger.info(String.format("Movie not found for ID: %s", id));
+            log.info(String.format("Movie not found for ID: %s", id));
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void deleteMovie(Movie movie) {
-        logger.info(String.format("Deleting movie with ID: %d", movie.getId()));
+        log.info(String.format("Deleting movie with ID: %d", movie.getId()));
         movieRepository.delete(Objects.requireNonNull(movie, "Movie cannot be null."));
-        logger.info("Movie deleted successfully");
+        log.info("Movie deleted successfully");
     }
 
     @Override
     public void saveMovie(Movie movie) {
         movieRepository.save(Objects.requireNonNull(movie, "Movie cannot be null."));
-        logger.info("Movie saved.");
+        log.info("Movie saved.");
     }
 }
