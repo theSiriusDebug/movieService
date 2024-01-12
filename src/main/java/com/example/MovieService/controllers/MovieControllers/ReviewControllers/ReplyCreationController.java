8ef23bc3f621af90e9a202881a1b0cff7bd48afe
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-12 04:40:16 +0200

    rebuild jar.

--- src/main/java/com/example/MovieService/controllers/MovieControllers/ReviewControllers/ReplyCreationController.java
+++ src/main/java/com/example/MovieService/controllers/MovieControllers/ReviewControllers/ReplyCreationController.java
@@ -18,7 +18,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @Slf4j
 @Api(tags = "ReviewCreationController API")
--- src/main/java/com/example/MovieService/models/Review.java
+++ src/main/java/com/example/MovieService/models/Review.java
@@ -3,8 +3,6 @@ package com.example.MovieService.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import javax.persistence.*;
