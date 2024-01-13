5fbc26969ae509193fe48149be47f1b0739dbda0
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-13 10:47:48 +0200

    MovieService added some new functions.

--- src/main/java/com/example/MovieService/sevices/interfaces/MovieService.java
+++ src/main/java/com/example/MovieService/sevices/interfaces/MovieService.java
@@ -1,8 +1,10 @@
 package com.example.MovieService.sevices.interfaces;
 
 import com.example.MovieService.models.Movie;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieService {
     List<Movie> findAllMovies();
@@ -12,4 +14,12 @@ public interface MovieService {
     void deleteMovie(Movie movie);
 
     void saveMovie(Movie movie);
+
+    List<Movie>findMovieByTitle(String title, Sort sorting);
+
+    List<Movie> findByMovieTitle(String title);
+
+    Optional<Movie> findMovieById(Long id);
+
+    List<Movie> findAllMoviesSorted(Sort sorting);
 }
