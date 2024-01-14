221b4e57f9d2af109d81c68acadfd0f4da407e2f
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-14 16:33:26 +0200

    changed UserProfileController.

--- src/main/java/com/example/MovieService/controllers/UserControllers/UserProfileController.java
+++ src/main/java/com/example/MovieService/controllers/UserControllers/UserProfileController.java
@@ -1,18 +1,34 @@
 package com.example.MovieService.controllers.UserControllers;
 
-import com.example.MovieService.models.dtos.UserRegistrationDto;
+import com.example.MovieService.models.User;
+import com.example.MovieService.sevices.UserServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "UserProfileController API")
 public class UserProfileController {
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserProfileController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
     @ApiOperation("Get user profile")
     @GetMapping("/profile")
-    public UserRegistrationDto getUserProfile() {
-        UserRegistrationDto userDto = new UserRegistrationDto();
-        return userDto;
+    public ResponseEntity<User> getUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        User currentUser = userService.findByOptionalUsername(username);
+
+        return ResponseEntity.ok(currentUser);
     }
 }
