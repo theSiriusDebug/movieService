a1badd64c54720ce29b5c525b626b5df26ccecf2
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-15 14:29:31 +0200

    added ReviewDto.

---
+++ src/main/java/com/example/MovieService/models/dtos/ReviewDto.java
@@ -0,0 +1,13 @@
+package com.example.MovieService.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewDto {
+    private String reviewText;
+    private String reviewOwner;
+}
