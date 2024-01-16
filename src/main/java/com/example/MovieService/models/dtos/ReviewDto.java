f02bff686837f9aa7f4897a1d1714cd8c0f83b67
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-16 15:57:21 +0200

    ReviewDto added field - replies.

--- src/main/java/com/example/MovieService/models/dtos/ReviewDto.java
+++ src/main/java/com/example/MovieService/models/dtos/ReviewDto.java
@@ -1,8 +1,11 @@
 package com.example.MovieService.models.dtos;
 
+import com.example.MovieService.models.Reply;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class ReviewDto {
     private String reviewText;
     private String reviewOwner;
+    private List<Reply>replies;
 }
