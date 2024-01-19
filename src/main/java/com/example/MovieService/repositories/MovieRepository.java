f967a6b054dd6694b11a22df0befad3450f25091
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-19 09:51:41 +0200

    Refactor search method in MovieRepository to use a custom query.

--- src/main/java/com/example/MovieService/repositories/MovieRepository.java
+++ src/main/java/com/example/MovieService/repositories/MovieRepository.java
@@ -3,6 +3,7 @@ package com.example.MovieService.repositories;
 import com.example.MovieService.models.Movie;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,6 @@ import java.util.Optional;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Movie findById(long id);
     Optional<Movie> findById(Long id);
-    List<Movie> findByTitleStartingWithIgnoreCase(String title);
-    List<Movie>findByTitleContainingIgnoreCase(String title, Sort sorting);
+    @Query("SELECT m FROM Movie m WHERE m.title LIKE %?1%")
+    List<Movie> search(String title, Sort sorting);
 }
