2a9a8de31029cc216a84860a443024d181ce6b05
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-23 23:23:45 +0200

    Removed Reply and all mentions.

--- src/main/java/com/example/MovieService/repositories/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MovieService.repositories;
-
-import com.example.MovieService.models.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
