9548cbdd73c8553ab79a938ee4450b34abc4c74f
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-25 00:27:28 +0200

    UserProfileController start using UserDto instead of User.

--- src/main/java/com/example/MovieService/controllers/UserControllers/UserProfileController.java
+++ src/main/java/com/example/MovieService/controllers/UserControllers/UserProfileController.java
@@ -1,7 +1,9 @@
 package com.example.MovieService.controllers.UserControllers;
 
 import com.example.MovieService.models.User;
+import com.example.MovieService.models.dtos.UserDto;
 import com.example.MovieService.sevices.UserServiceImpl;
+import com.example.MovieService.utils.validator.UserMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +25,10 @@ public class UserProfileController {
 
     @ApiOperation("Get user profile")
     @GetMapping("/profile")
-    public ResponseEntity<User> getUserProfile() {
+    public ResponseEntity<UserDto> getUserProfile() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User currentUser = userService.findByOptionalUsername(authentication.getName());
 
-        return ResponseEntity.ok(currentUser);
+        return ResponseEntity.ok(UserMapper.mapToUserDto(currentUser));
     }
 }
