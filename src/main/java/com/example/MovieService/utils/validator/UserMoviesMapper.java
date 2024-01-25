4df2f2761275f7ef5e5abdba82e7eaa18ab26093
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-25 09:49:30 +0200

    Added UserMoviesDto and UserMoviesMapper.

---
+++ src/main/java/com/example/MovieService/utils/validator/UserMoviesMapper.java
@@ -0,0 +1,15 @@
+package com.example.MovieService.utils.validator;
+
+import com.example.MovieService.models.Movie;
+import com.example.MovieService.models.dtos.UserMoviesDto;
+
+public class UserMoviesMapper {
+    public static UserMoviesDto mapToUserMovieDto(Movie movie){
+        return new UserMoviesDto(
+                movie.getTitle(),
+                movie.getImdbRating(),
+                movie.getKinopoiskRating(),
+                movie.getPosterLink()
+        );
+    }
+}
