4938e0f6256142807ac59546e3684e7935e98d2f
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-28 15:23:57 +0200

    Change Role entity.

--- src/main/java/com/example/MovieService/models/Role.java
+++ src/main/java/com/example/MovieService/models/Role.java
@@ -1,25 +1,19 @@
 package com.example.MovieService.models;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "reviewText must not be blank")
     @Min(value = 1)
     private String name;
-
-    public Role(String name) {
-        super();
-        this.name = name;
-    }
 }
