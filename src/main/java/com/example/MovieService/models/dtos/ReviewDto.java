15ecd129b1808d86b3bb4ad29da6177c451a9300
theSiriusDebug <misha.ch2006@gmail.com>
2024-01-31 18:25:25 +0200

    Change ReviewDto.

--- src/main/java/com/example/MovieService/models/dtos/ReviewDto.java
+++ src/main/java/com/example/MovieService/models/dtos/ReviewDto.java
@@ -1,6 +1,5 @@
 package com.example.MovieService.models.dtos;
 
-import com.example.MovieService.models.Review;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +12,5 @@ import java.util.List;
 public class ReviewDto {
     private String reviewText;
     private String reviewOwner;
-    private List<Review>replies;
+    private List<ReviewDto>replies;
 }
--- src/main/java/com/example/MovieService/utils/mappers/ReviewMapper.java
+++ src/main/java/com/example/MovieService/utils/mappers/ReviewMapper.java
@@ -2,13 +2,16 @@ package com.example.MovieService.utils.mappers;
 
 import com.example.MovieService.models.Review;
 import com.example.MovieService.models.dtos.ReviewDto;
+import jakarta.validation.Valid;
+
+import java.util.stream.Collectors;
 
 public class ReviewMapper {
-    public static ReviewDto mapToReviewDto(Review review){
+    public static ReviewDto mapToReviewDto(@Valid Review review){
         return new ReviewDto(
                 review.getReviewText(),
                 review.getUser().getUsername(),
-                review.getReplies()
+                review.getReplies().stream().map(ReviewMapper::mapToReviewDto).collect(Collectors.toList())
         );
     }
 }
