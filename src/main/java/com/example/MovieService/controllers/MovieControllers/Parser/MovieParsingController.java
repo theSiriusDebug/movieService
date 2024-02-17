d38dbd250550e0a5600de2d31f4fcdc15b57334f
theSiriusDebug <misha.ch2006@gmail.com>
2024-02-17 06:04:11 +0200

    add Multithreading into parser.

--- src/main/java/com/example/MovieService/controllers/MovieControllers/Parser/MovieParsingController.java
+++ src/main/java/com/example/MovieService/controllers/MovieControllers/Parser/MovieParsingController.java
@@ -8,36 +8,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
 @Api(tags = "ParserController API")
 public class MovieParsingController {
     private final MovieDetailsExtractor movieDetailsExtractor;
+    private final ExecutorService executorService;
 
     @Autowired
     public MovieParsingController(MovieDetailsExtractor movieDetailsExtractor) {
-
         this.movieDetailsExtractor = movieDetailsExtractor;
+        this.executorService = Executors.newFixedThreadPool(5);
     }
 
     @ApiOperation("Parse movie details")
     @PostMapping("/parse")
     public void parseMovies() {
-        long latestPage = 1;
+        long latestPage = 10;
         long currentPage = 1;
 
         while (currentPage <= latestPage) {
-            try {
-                String url = String.format("https://m.anwap.love/films/%d", currentPage);
-                String html = movieDetailsExtractor.fetchHtml(url);
-                movieDetailsExtractor.parseMovieDetails(html);
-                log.info("Successfully parsed page {}", currentPage);
-            } catch (IOException e) {
-                log.info("Error parsing page {}", currentPage);
-                throw new RuntimeException(e);
-            }
+            final long page = currentPage;
+            executorService.submit(() -> parseMoviePage(page));
             currentPage++;
+        }
+
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            log.info("All parsing tasks completed.");
+        } catch (InterruptedException e) {
+            log.error("Error waiting for parsing tasks to complete: {}", e.getMessage());
+        }
+    }
+
+    private void parseMoviePage(long page) {
+        try {
+            String url = String.format("https://m.anwap.love/films/%d", page);
+            String html = movieDetailsExtractor.fetchHtml(url);
+            movieDetailsExtractor.parseMovieDetails(html);
+            log.info("Successfully parsed page {}", page);
+        } catch (IOException e) {
+            log.info("Error parsing page {}", page);
+            throw new RuntimeException(e);
         }
     }
 }
