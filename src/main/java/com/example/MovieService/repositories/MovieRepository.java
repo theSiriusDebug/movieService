2f4d1220ae7a699e7e7fb696564c12d70b250479
theSiriusDebug <misha.ch2006@gmail.com>
2024-03-01 19:57:03 +0200

    MovieRepository removed redundant method.

--- src/main/java/com/example/MovieService/repositories/MovieRepository.java
+++ src/main/java/com/example/MovieService/repositories/MovieRepository.java
@@ -1,18 +1,13 @@
 package com.example.MovieService.repositories;
 
 import com.example.MovieService.models.Movie;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Movie findById(long id);
     Optional<Movie> findById(Long id);
-    @Query("SELECT m FROM Movie m WHERE m.title LIKE %?1%")
-    List<Movie> search(String title, Sort sorting);
 }
